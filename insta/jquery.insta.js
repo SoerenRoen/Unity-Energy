7b8e578f1a985c500656f5ae7a5ef3cf7cd1af7a
Søren Vindfelt Røn <SoerenRoen@Srens-MacBook-Pro.local>
2020-03-24 17:46:12 +0100

    xcv

---
+++ insta/jquery.insta.js
@@ -0,0 +1,9 @@
+$(window).on('load', function () {
+    $.instagramFeed({
+        'username': 'soerenroen',
+        'container': "#instagram-feed-demo",
+        'display_profile': "true",
+        'display_biography': "true",
+        'display_igtv': "false",
+    });
+});
